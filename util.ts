18d79151c12bf6d7f8d3cddb76d74dfe7f1ae0c4
Arbaaz-Chouha <chouhanarbaaz4@gmail.com>
2025-12-26 18:11:27 +0530

    working  on story page

--- util.ts
+++ util.ts
@@ -1,12 +1,12 @@
 function createApiUrl(url: string) {
-    // __DEV__ is true if we using localhost and false on production
-    if (__DEV__) {
-      console.log(`${process.env.EXPO_PUBLIC_LOCAL_API_URL}${url}`);
-      
-        return `http://88.222.213.106:3000/api${url}`;
-        
-    }
-    return `$http://88.222.213.106:3000/api${url}`;
+  // __DEV__ is true if we using localhost and false on production
+  if (__DEV__) {
+    console.log(`${process.env.EXPO_PUBLIC_LOCAL_API_URL}/api${url}`);
+
+    return `http://88.222.213.106:3000/api${url}`;
+
+  }
+  return `$http://88.222.213.106:3000/api${url}`;
 }
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
