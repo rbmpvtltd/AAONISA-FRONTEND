f5cbd5d98ce8e611756d953e3c50513636285bdc
Adanan-Chouhan <adananchouhan@gmail.com>
2025-12-31 12:15:14 +0530

    reels feed page fix tabes potion and background and stutus baar set and reels feed, mention feed , postfeed page icon changes

--- app/p/[username]/[id]/mention.tsx
+++ app/p/[username]/[id]/mention.tsx
@@ -366,7 +366,7 @@ const MentionedReelItem = ({
             }
           });
         }}>
-          <Ionicons name="share-social-outline" size={ACTION_ICON_SIZE} color="#fff" />
+          <Ionicons name="paper-plane-outline" size={ACTION_ICON_SIZE} color="#fff" />
           <Text style={styles.actionText}>{formatCount(item.shares?.length || 0)}</Text>
         </TouchableOpacity>
 
